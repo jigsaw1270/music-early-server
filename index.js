7b8d8d9b9138766c07b7e76c6014f709b3c892cb
jigsaw1270 <nomadshomoy@gmail.com>
2023-06-10 23:05:34 +0600

    role added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // 3DWky1ONivTIPBqn
 // musically
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jigsaw1270.6chjsjt.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -36,13 +36,56 @@ async function run() {
     const instructorsCollection = client.db("musicallyDb").collection("instructors");
     const usersCollection = client.db("musicallyDb").collection("users");
 
+    // users
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-    
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
       const result = await usersCollection.insertOne(user);
-      console.log(result);
-      res.send(result);})
+      res.send(result);
+    });
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
 
     app.get('/classes', async (req, res) => {
